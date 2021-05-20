87b19bb34f92d4cf9476f61b8c8e5c70c754394f
Rajashekhar Vanahalli <rajashekhar.vanahalli@cricbuzz.com>
2021-05-20 15:50:27 +0530

    Changed data access to repo

--- app/src/main/java/com/target/targetcasestudy/ui/deals/DealsViewModel.kt
+++ app/src/main/java/com/target/targetcasestudy/ui/deals/DealsViewModel.kt
@@ -3,15 +3,15 @@ package com.target.targetcasestudy.ui.deals
 import com.target.targetcasestudy.common.ActionableLiveData
 import com.target.targetcasestudy.common.BaseLiveData
 import com.target.targetcasestudy.common.ObservableData
-import com.target.targetcasestudy.data.repository.network.Api
 import com.target.targetcasestudy.data.repository.network.DealsResponse
+import com.target.targetcasestudy.data.repository.network.Repo
 import com.target.targetcasestudy.data.utils.async
 import com.target.targetcasestudy.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class DealsViewModel @Inject constructor(private val api: Api): BaseViewModel() {
+class DealsViewModel @Inject constructor(private val repo: Repo): BaseViewModel() {
 
     private val deals = disposableLiveData {
         ActionableLiveData<DealsResponse>()
@@ -20,7 +20,7 @@ class DealsViewModel @Inject constructor(private val api: Api): BaseViewModel() 
     fun deals(): BaseLiveData<ObservableData<DealsResponse, Throwable>> =
         deals.apply {
             actionBlock = {
-                api
+                repo
                     .deals()
                     .async()
             }
--- app/src/main/java/com/target/targetcasestudy/ui/dealsitem/DealsItemViewModel.kt
+++ app/src/main/java/com/target/targetcasestudy/ui/dealsitem/DealsItemViewModel.kt
@@ -3,15 +3,15 @@ package com.target.targetcasestudy.ui.dealsitem
 import com.target.targetcasestudy.common.ActionableLiveData
 import com.target.targetcasestudy.common.BaseLiveData
 import com.target.targetcasestudy.common.ObservableData
-import com.target.targetcasestudy.data.repository.network.Api
 import com.target.targetcasestudy.data.repository.network.Product
+import com.target.targetcasestudy.data.repository.network.Repo
 import com.target.targetcasestudy.data.utils.async
 import com.target.targetcasestudy.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class DealsItemViewModel @Inject constructor(private val api: Api): BaseViewModel() {
+class DealsItemViewModel @Inject constructor(private val repo: Repo): BaseViewModel() {
 
     private val dealsItem = disposableLiveData {
         ActionableLiveData<Product>()
@@ -20,7 +20,7 @@ class DealsItemViewModel @Inject constructor(private val api: Api): BaseViewMode
     fun dealItem(id: Int): BaseLiveData<ObservableData<Product, Throwable>> =
         dealsItem.apply {
             actionBlock = {
-                api
+                repo
                     .dealsItem(id)
                     .async()
             }
